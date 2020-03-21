f365f3b422c3b6035e02722ef19812455db9f894
Prince9821 <1043113120@qq.com>
2020-03-21 07:24:38 -0700

    first

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -16,18 +16,20 @@ int64_t now()
 int main(int argc, char* argv[])
 {
   init_neighbors();
-
   FILE* fp = fopen(argv[1], "r");
+  while(scanf(fp,"%s",argv[1])!=EOF){
+    FILE* fp = fopen(argv[1], "r");
+  }
   char puzzle[128];
   int total_solved = 0;
   int total = 0;
-  bool (*solve)(int) = solve_sudoku_basic;
+  bool (*solve)(int) = solve_sudoku_dancing_links;
   if (argv[2] != NULL)
-    if (argv[2][0] == 'a')
+    /*if (argv[2][0] == 'a')
       solve = solve_sudoku_min_arity;
     else if (argv[2][0] == 'c')
       solve = solve_sudoku_min_arity_cache;
-    else if (argv[2][0] == 'd')
+    else if (argv[2][0] == 'd')*/
       solve = solve_sudoku_dancing_links;
   int64_t start = now();
   while (fgets(puzzle, sizeof puzzle, fp) != NULL) {
