ba2095333a7dc89de3383478142632f5ecebbc09
cscwxu <601240746@qq.com>
2020-03-28 01:39:34 +0800

    basic

--- Lab1/src/Sudoku/main.cc
+++ Lab1/src/Sudoku/main.cc
@@ -17,14 +17,13 @@ int main(int argc, char* argv[])
 {
 	char fileName[128];
 	while(scanf("%s",fileName)!=EOF){
-  	init_neighbors();
+  	//init_neighbors();
 		FILE* fp =fopen(fileName,"r");
   	//FILE* fp = fopen(argv[1], "r");
   	char puzzle[128];
   	int total_solved = 0;
   	int total = 0;
-  	bool (*solve)(int) = solve_sudoku_basic;
-  	solve=solve_sudoku_dancing_links;
+  	bool (*solve)(int) = solve_sudoku_dancing_links;
   	/*
   	if (argv[2] != NULL)
     if (argv[2][0] == 'a')
@@ -39,7 +38,7 @@ int main(int argc, char* argv[])
     	if (strlen(puzzle) >= N) {
       	++total;
       	input(puzzle);
-      	init_cache();
+      	//init_cache();
       	//if (solve_sudoku_min_arity_cache(0)) {
       	//if (solve_sudoku_min_arity(0))
       	//if (solve_sudoku_basic(0)) {
